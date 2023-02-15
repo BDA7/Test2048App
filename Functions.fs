95bb5c6f72a5368330562c7381702cee45543b55
BDA7 <bondare-danila2017@yandex.ru>
2023-02-15 13:15:48 +0300

    first commit

---
+++ Functions.fs
@@ -0,0 +1,102 @@
+﻿module Functions
+
+open System
+
+let rec shifLeftUp (arr: string[]) (value: string) (i: int) =
+    if (i < 0) || (arr.[i] <> "") then
+        (arr)
+    else
+        arr.[i + 1] <- ""
+        arr.[i] <- value
+        shifLeftUp arr value (i - 1)
+
+
+let rec shiftRightDown (arr: string[]) (value: string) (i: int) (size: int) =
+    if (i > size - 1) || (arr.[i] <> "") then
+        (arr)
+    else
+        arr.[i] <- value
+        arr.[i - 1] <- ""
+        shiftRightDown arr value (i + 1) size
+
+
+
+let shiftLeft (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in 0 .. (sizeLine - 1) do
+            if elemsArr.[i, j] <> "" then
+                elemsArr.[i, *] <- shifLeftUp elemsArr.[i, *] elemsArr.[i, j] (j - 1)
+
+    (elemsArr)
+
+
+let shiftRight (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in (sizeLine - 1) .. -1 .. 0 do
+            if (elemsArr.[i, j] <> "") then
+                elemsArr.[i, *] <- shiftRightDown elemsArr.[i, *] elemsArr.[i, j] (j + 1) sizeLine
+
+    (elemsArr)
+
+
+let shiftUp (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in 0 .. (sizeLine - 1) do
+            if (elemsArr.[j, i] <> "") then
+                elemsArr.[*, i] <- shifLeftUp elemsArr.[*, i] elemsArr.[j, i] (j - 1)
+
+    (elemsArr)
+
+
+let shiftDown (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in (sizeLine - 1) .. -1 .. 0 do
+            if (elemsArr.[j, i] <> "") then
+                elemsArr.[*, i] <- shiftRightDown elemsArr.[*, i] elemsArr.[j, i] (j + 1) sizeLine
+
+    (elemsArr)
+
+
+
+let sumLeft (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in 0 .. (sizeLine - 2) do
+            if (elemsArr.[i, j] = elemsArr.[i, j + 1]) && (elemsArr.[i, j] <> "") then
+                let value = elemsArr.[i, j] |> int
+                elemsArr.[i, j] <- (value * 2).ToString()
+                elemsArr.[i, j + 1] <- ""
+
+    (elemsArr)
+
+
+let sumRight (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in (sizeLine - 1) .. -1 .. 1 do
+            if (elemsArr.[i, j] = elemsArr.[i, j - 1]) && (elemsArr.[i, j] <> "") then
+                let value = elemsArr.[i, j] |> int
+                elemsArr.[i, j] <- (value * 2).ToString()
+                elemsArr.[i, j - 1] <- ""
+
+    (elemsArr)
+
+
+let sumUp (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in 0 .. (sizeLine - 2) do
+            if (elemsArr.[j, i] = elemsArr.[j + 1, i]) && (elemsArr.[j, i] <> "") then
+                let value = elemsArr.[j, i] |> int
+                elemsArr.[j, i] <- (value * 2).ToString()
+                elemsArr.[j + 1, i] <- ""
+
+    (elemsArr)
+
+
+let sumDown (elemsArr: string[,]) (sizeLine: int) =
+    for i in 0 .. (sizeLine - 1) do
+        for j in (sizeLine - 1) .. -1 .. 1 do
+            if (elemsArr.[j, i] = elemsArr.[j - 1, i]) && (elemsArr.[j, i] <> "") then
+                let value = elemsArr.[j, i] |> int
+                elemsArr.[j, i] <- (value * 2).ToString()
+                elemsArr.[j - 1, i] <- ""
+
+    (elemsArr)
---
+++ Tests.fs
@@ -0,0 +1,115 @@
+﻿module Tests
+
+open System
+open Xunit
+open Functions
+
+let createArr () =
+    let arr2D = Array2D.create 5 5 ""
+    arr2D.[0, *] <- [| ""; "4"; ""; "8"; "4" |]
+    arr2D.[1, *] <- [| "4"; ""; ""; "8"; "4" |]
+    arr2D.[3, *] <- [| ""; ""; "8"; "8"; "" |]
+    arr2D.[4, *] <- [| "2"; "4"; "8"; "8"; "4" |]
+    (arr2D)
+
+[<Fact>]
+let ``shiftLeft Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| "4"; "8"; "4"; ""; "" |]
+    arr2DCorrect.[1, *] <- [| "4"; "8"; "4"; ""; "" |]
+    arr2DCorrect.[3, *] <- [| "8"; "8"; ""; ""; "" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; "8"; "8"; "4" |]
+    let useFunc = shiftLeft arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+
+[<Fact>]
+let ``shiftRight Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| ""; ""; "4"; "8"; "4" |]
+    arr2DCorrect.[1, *] <- [| ""; ""; "4"; "8"; "4" |]
+    arr2DCorrect.[3, *] <- [| ""; ""; ""; "8"; "8" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; "8"; "8"; "4" |]
+    let useFunc = shiftRight arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+[<Fact>]
+let ``shiftUp Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| "4"; "4"; "8"; "8"; "4" |]
+    arr2DCorrect.[1, *] <- [| "2"; "4"; "8"; "8"; "4" |]
+    arr2DCorrect.[2, *] <- [| ""; ""; ""; "8"; "4" |]
+    arr2DCorrect.[3, *] <- [| ""; ""; ""; "8"; "" |]
+    arr2DCorrect.[4, *] <- [| ""; ""; ""; ""; "" |]
+    let useFunc = shiftUp arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+[<Fact>]
+let ``shiftDown Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| ""; ""; ""; ""; "" |]
+    arr2DCorrect.[1, *] <- [| ""; ""; ""; "8"; "" |]
+    arr2DCorrect.[2, *] <- [| ""; ""; ""; "8"; "4" |]
+    arr2DCorrect.[3, *] <- [| "4"; "4"; "8"; "8"; "4" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; "8"; "8"; "4" |]
+    let useFunc = shiftDown arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+
+[<Fact>]
+let ``sumLeft Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| ""; "4"; ""; "8"; "4" |]
+    arr2DCorrect.[1, *] <- [| "4"; ""; ""; "8"; "4" |]
+    arr2DCorrect.[3, *] <- [| ""; ""; "16"; ""; "" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; "16"; ""; "4" |]
+    let useFunc = sumLeft arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+[<Fact>]
+let ``sumRight Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| ""; "4"; ""; "8"; "4" |]
+    arr2DCorrect.[1, *] <- [| "4"; ""; ""; "8"; "4" |]
+    arr2DCorrect.[3, *] <- [| ""; ""; ""; "16"; "" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; ""; "16"; "4" |]
+    let useFunc = sumRight arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+[<Fact>]
+let ``sumUp Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| ""; "4"; ""; "16"; "8" |]
+    arr2DCorrect.[1, *] <- [| "4"; ""; ""; ""; "" |]
+    arr2DCorrect.[2, *] <- [| ""; ""; ""; ""; "" |]
+    arr2DCorrect.[3, *] <- [| ""; ""; "16"; "16"; "" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; ""; ""; "4" |]
+    let useFunc = sumUp arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
+
+[<Fact>]
+let ``sumDown Test`` () =
+    let arr2D = createArr ()
+
+    let arr2DCorrect = Array2D.create 5 5 ""
+    arr2DCorrect.[0, *] <- [| ""; "4"; ""; ""; "" |]
+    arr2DCorrect.[1, *] <- [| "4"; ""; ""; "16"; "8" |]
+    arr2DCorrect.[2, *] <- [| ""; ""; ""; ""; "" |]
+    arr2DCorrect.[3, *] <- [| ""; ""; ""; ""; "" |]
+    arr2DCorrect.[4, *] <- [| "2"; "4"; "16"; "16"; "4" |]
+    let useFunc = sumDown arr2D 5
+    Assert.Equal(arr2DCorrect, useFunc)
